c73efdf2f6192b8de7f0c3f542539c796c8f3225
5pence <sbarriball@me.com>
2018-11-23 19:05:19 +0000

    added allowed host to setting

--- newsproj/settings.py
+++ newsproj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['unicorn-attractor.herokuapp.com/']
+ALLOWED_HOSTS = ['unicorn-attractor.herokuapp.com']
 
 
 # Application definition
